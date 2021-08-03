92a17a9862635fbabef25c3e3c6ebad901bc7d23
dyje <dyje@oocl.com>
2021-08-04 00:01:39 +0800

    [PosMachine][Jesse]feat: add first function getItems

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,36 @@
 package pos.machine;
 
-import java.util.List;
+import java.util.*;
+
 
 public class PosMachine {
+
     public String printReceipt(List<String> barcodes) {
         return null;
+    }
+    private List<Item> getItems(List<String> barcodes) {
+        LinkedList<Item> items = new LinkedList<Item>();
+        List<ItemInfo> itemInfo = loadAllItemsInfo();
+        barcodes = new ArrayList<>(new LinkedHashSet<>(barcodes));
+        for (String barcode : barcodes) {
+            Item itemValue = new Item();
+            for (ItemInfo itemInfoVal : itemInfo) {
+                if (barcode.equals(itemInfoVal.getBarcode())) {
+                    itemValue.setName(itemInfoVal.getName());
+                    itemValue.setUnitPrice(itemInfoVal.getPrice());
+                    itemValue.setQuantity(retrieveItemCount(barcode));
+                }
+            }
+            items.add(itemValue);
+        }
+        return items;
+    }
+
+    private int retrieveItemCount(String currentItemBarcode) {
+        return Collections.frequency(ItemDataLoader.loadBarcodes(), currentItemBarcode);
+    }
+
+        private List<ItemInfo> loadAllItemsInfo(){
+        return ItemDataLoader.loadAllItemInfos();
     }
 }
